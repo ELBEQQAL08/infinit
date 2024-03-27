87bf8d1366d95f916be8ae47fb10c8df076c0b53
Brad <elbeqqal.kamal@gmail.com>
2024-03-28 00:51:58 +0100

    first commit

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const menu_mob = document.querySelector(".menu");
 
 console.log(menu_mob);
 
-const close_mob = document.querySelector(".close");
+const close_mob = document.querySelector(".close-display");
 
 console.log(close_mob);
 
 menu_mob.addEventListener("click", () => {
-  close_mob.style.visibility = "visible";
+  close_mob.style.display = "block";
   menu_mob.style.visibility = "hidden";
 });
 
