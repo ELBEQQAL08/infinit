7f081d15095e4b73869c4a8cd816100ee81be729
Brad <elbeqqal.kamal@gmail.com>
2024-04-01 20:25:48 +0200

    another change

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const links__trans = document.querySelector(".links__trans_mob");
 
 menu_mob.addEventListener("click", () => {
   links_mob.classList.toggle("links-mob");
-  links__trans.classList.toggle("links_trans");
+  links__trans.classList.toggle("links__trans");
 });
