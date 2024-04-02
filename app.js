e99c3bc15ca7b1d6479ded1ba526c4c7a11cf6e0
Brad <elbeqqal.kamal@gmail.com>
2024-04-02 15:26:58 +0200

    another change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// const menu_mob = document.querySelector(".menu");
-// let links_mob = document.querySelector(".links-mob");
+const menu_mob = document.querySelector(".menu");
+let links_mob = document.querySelector(".links_mob");
 
-// menu_mob.addEventListener("click", () => {
-//   // links_mob.classList.toggle("links-mob");
-// });
+menu_mob.addEventListener("click", () => {
+  links_mob.classList.toggle("links_mob");
+});
