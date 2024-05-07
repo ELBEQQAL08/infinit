6bb1c1c92ec37261fc57965545f91a8657e8a331
Brad <elbeqqal.kamal@gmail.com>
2024-05-08 00:34:45 +0200

    last change

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const menu__mob = document.querySelector(".menu");
 const links__mob = document.querySelector(".links__mob");
 const links__hidden = document.querySelector(".links__visible");
 const close__visible = document.querySelector(".close");
+<<<<<<< HEAD
 menu__mob.style.cursor = "pointer";
 
 menu__mob.addEventListener("click", () => {
@@ -13,6 +14,24 @@ menu__mob.addEventListener("click", () => {
 close__visible.addEventListener("click", () => {
   links__mob.classList.toggle("links__mob");
   links__hidden.classList.remove(".links__visible");
+=======
+const container = document.querySelector(".links-mob__container");
+menu__mob.style.cursor = "pointer";
+close__visible.style.cursor = "pointer";
+
+// Visible Links And Remove ClassName("links__mob")
+
+menu__mob.addEventListener("click", () => {
+  container.classList.add("links__visible");
+  container.classList.remove("links__mob");
+});
+
+// Hide Links And Remove ClassName("links__visible")
+
+close__visible.addEventListener("click", () => {
+  container.classList.add("links__mob");
+  container.classList.remove("links__visible");
+>>>>>>> 08e107f (last commit)
 });
 
 // Second Section
