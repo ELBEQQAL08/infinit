1ba7441fe35ac297861ce9e802273b60f657ab7b
Brad <elbeqqal.kamal@gmail.com>
2024-06-16 17:06:31 +0200

    last change

--- app.js
+++ app.js
@@ -74,15 +74,11 @@ btn__submit.addEventListener("click", () => {
 
 function ErrorValue() {
   try {
-    input__text_1.value;
-    input__text_2.value;
-    input__text_3.value;
+    input__text_1.value = value;
+    input__text_2.value = value;
+    input__text_3.value = value;
   } catch (e) {
-    input__text_1.value =
-      "An error occurred while sending the message. please try again";
-    input__text_2.value =
-      "An error occurred while sending the message. please try again";
-    input__text_3.value =
+    btn__submit.value =
       "An error occurred while sending the message. please try again";
   }
 }
