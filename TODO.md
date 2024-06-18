fb9575a2b96c45da5551c9e47ba5945e1678eaba
Brad <elbeqqal.kamal@gmail.com>
2024-06-18 19:51:46 +0200

    last change

--- TODO.md
+++ TODO.md
@@ -1,5 +1,5 @@
-- [x] Working Contact Page With Mobile And Meduim
-- [x] Working Event Submit In Section Contact Page
-- [x] Fixe Errors In Project
-- [ ] Working Message Error In Contact And Working Form Validation
-- [ ] Working Cercle With Javascript
+- 1 [x] Working Contact Page With Mobile And Meduim
+- 2 [x] Working Event Submit In Section Contact Page
+- 3 [x] Fixe Errors In Project
+- 4 [ ] Working Message Error In Contact And Working Form Validation
+- 5 [ ] Working Cercle With Javascript
--- app.js
+++ app.js
@@ -61,18 +61,56 @@ const btn__submit = document.querySelector("#btn__event");
 const input__text_1 = document.querySelector(".class__1");
 const input__text_2 = document.querySelector(".class__2");
 const input__text_3 = document.querySelector(".class__3");
+let form__Input = document.querySelector(".form__input");
 
-function ShowValue() {
+// Function ShowValue In Contact
+
+function showValue(event) {
   console.log(input__text_1.value);
   console.log(input__text_2.value);
   console.log(input__text_3.value);
+  event.preventDefault();
 }
 
-btn__submit.addEventListener("click", () => {
-  ShowValue();
-});
+btn__submit.addEventListener("click", showValue);
 
-input__text_1.addEventListener("click", (e) => {
-  e.parentElement();
-  validateInputs();
-});
+// Function Message Error And Validation Success !
+
+function showError(errorElement, errorMessage) {
+  document.querySelector("." + errorElement).classList.add("display-error");
+  document.querySelector("." + errorElement).innerHTML = errorMessage;
+}
+
+function clearError() {
+  let errors = document.querySelectorAll(".error");
+  for (let error of errors) {
+    error.classList.remove("display-error");
+  }
+}
+
+form__Input.onsubmit = function (event) {
+  clearError();
+
+  if (form__Input.name.value === "") {
+    showError("name-error", "You have to enter your name");
+    return false;
+  }
+
+  if (form__Input.email.value === "") {
+    showError("email-error", "You have to enter your email");
+    return false;
+  }
+
+  if (form__Input.message.value === "") {
+    showError("message-error", "You have to enter your message");
+    return false;
+  }
+
+  document.querySelector(".success").classList.add("display-success");
+  document.querySelector(".success").innerHTML =
+    "Your registration was successful.";
+
+  event.preventDefault();
+};
+
+// Cercle Top Direction
